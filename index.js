7d5c3ba3c539d5786381a5534b93e4089c007e72
Dani Mizuta <dani.mizuta@gmail.com>
2024-12-31 00:00:13 -0300

    feat:

--- index.js
+++ index.js
@@ -116,10 +116,10 @@ app.post('/login', async (req, res) => {
         // If authentication is successful, return user data (including cadastro id) and generate JWT token
         //const token = jwt.sign({ username: user.username, role: user.role, id: user.id }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
 
-        const token = jwt.sign({ username: user.username, role: user.role, customerIdid: customerIdid }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
+        const token = jwt.sign({ username: user.username, role: user.role, customerId: customerId }, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
 
         // Send the response with the token
-        res.json({ success: true, message: 'Login successful.', user: { username: user.username, role: user.role, id: id }, token });
+        res.json({ success: true, message: 'Login successful.', user: { username: user.username, role: user.role, customerId: customerId }, token });
 
     } catch (error) {
         console.error('Error during login:', error);
