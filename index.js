fa22f94796012411b2da27746ef8f680b1e51155
Dani Mizuta <dani.mizuta@gmail.com>
2025-01-04 16:02:21 -0300

    feat:

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.get('/product-buy/:id', async (req, res) => {
     const productCode = req.params.id;
     try {
         const result = await pool.query('SELECT descricao, cxfechada, precofechada, precofrac, cxfracionada FROM produtos WHERE codproduto = $1', [productCode]);
-        res.json(result.rows,);
+        res.json(result.rows);
         console.log(result.rows);
 
 
