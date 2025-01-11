d8fe39c631e028455c1f689edda789151b088e4e
Dmizuta <dani.mizuta@gmail.com>
2025-01-11 14:09:38 -0300

    feat:

--- index.js
+++ index.js
@@ -426,13 +426,13 @@ app.get('/orders', async (req, res) => {
 
 
 // Endpoint to fetch orders for a specific username
-/*app.get('/orders-admin', async (req, res) => {
-    const { username } = req.query;
+app.get('/orders-admin', async (req, res) => {
+  /*  const { username } = req.query;
 
     if (!username) {
         return res.status(400).json({ message: 'NECESSÁRIO USUÁRIO.' });
-    }*/
-
+    }
+*/
     try {
 
 
