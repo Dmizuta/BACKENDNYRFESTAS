9092a69531d0bb174056ef20684a386ff2dbfe10
Dani Mizuta <dani.mizuta@gmail.com>
2025-02-01 18:52:26 -0300

    feat:

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get('/test-db-connection', async (req, res) => {
 // Endpoint to get products from the database
 app.get('/products', async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM produtos ORDER BY id ASC'); 
+        const result = await pool.query('SELECT * FROM produtos ORDER BY idprod ASC');
         res.json(result.rows);
     } catch (error) {
         res.status(500).json({
@@ -60,9 +60,6 @@ app.get('/products', async (req, res) => {
         });
     }
 });
-
-
-
 
 
 
