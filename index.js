1c6b349c1be9ae20e8d065a163f9f0b5c3c77fb4
Dani Mizuta <dani.mizuta@gmail.com>
2025-02-01 23:49:33 -0300

    feat:

--- index.js
+++ index.js
@@ -1035,7 +1035,7 @@ app.patch('/editproduct/:productId', async (req, res) => {
 
         // Calculate the new total for the order
         const totalResult = await pool.query(
-            'SELECT SUM(quantidade * preco + (ipi * quantidade * preco)) AS total FROM pedidoitens WHERE idpedido = $1',
+            'SELECT COALESCE(SUM(quantidade * preco * (1 + ipi * 0.13)), 0) AS total FROM pedidoitens WHERE idpedido = $1',
             [idPedido]
         );
 
