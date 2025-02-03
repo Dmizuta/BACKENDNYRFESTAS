da23f719fa8391f881da255ee29700f82a2bde81
Dani Mizuta <dani.mizuta@gmail.com>
2025-02-03 20:04:25 -0300

    feat: ipi

--- index.js
+++ index.js
@@ -1570,3 +1570,23 @@ app.post('/finishOrder', async (req, res) => {
         return res.status(500).json({ error: 'Internal server error.' });
     }
 });*/
+
+
+
+
+app.delete("/deleteCustomer/:id", async (req, res) => {
+    const customerId = req.params.id;
+
+    try {
+        const result = await pool.query("DELETE FROM cadastro WHERE id = $1", [customerId]);
+
+        if (result.rowCount > 0) {
+            res.json({ success: true, message: "Customer deleted" });
+        } else {
+            res.json({ success: false, message: "Customer not found" });
+        }
+    } catch (error) {
+        console.error("Error deleting customer:", error);
+        res.status(500).json({ success: false, message: "Server error" });
+    }
+});
