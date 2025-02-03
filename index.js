a108d9c664171196272f7acc334d261f0cbc5427
Dmizuta <dani.mizuta@gmail.com>
2025-02-03 12:09:54 -0300

    feat:

--- index.js
+++ index.js
@@ -1417,6 +1417,49 @@ app.patch("/revertOrder", async (req, res) => {
 
 
 
+
+
+
+
+app.patch("/receiveOrder", async (req, res) => {
+    const { orderId } = req.body;
+
+    if (!orderId) {
+        return res.status(400).json({ error: "Order ID is required." });
+    }
+
+    try {
+       
+
+        // Perform a single UPDATE query
+        const updateQuery = `
+            UPDATE pedidos 
+            SET status = 3
+            RETURNING *;
+        `;
+
+        const updateResult = await pool.query(updateQuery, [orderId]);
+
+        if (updateResult.rows.length > 0) {
+            return res.status(200).json({ message: "Order updated successfully." });
+        } else {
+            return res.status(500).json({ error: "Failed to update order." });
+        }
+    } catch (error) {
+        console.error("Error updating order:", error);
+        return res.status(500).json({ error: "Internal server error." });
+    }
+});
+
+
+
+
+
+
+
+
+
+
 app.patch("/finishOrder", async (req, res) => {
     const { orderId, observation } = req.body;
 
