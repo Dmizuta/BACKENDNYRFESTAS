cc8a19a5bed2c99849755de275a33a28a8f2b2c1
Dani Mizuta <dani.mizuta@gmail.com>
2025-03-08 14:30:33 -0300

    feat:discount

--- index.js
+++ index.js
@@ -922,7 +922,7 @@ app.post("/submit-order", async (req, res) => {
     try {
         const updateQuery = `
             UPDATE pedidos 
-            SET observacoes = "teste"
+            SET observacoes = $1, ipitotal = $2
             WHERE id = $3;
         `;
 
