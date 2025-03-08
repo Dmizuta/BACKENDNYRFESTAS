ab6aedc20f21556e0f7a4eae73bc1ebcfd758779
Dani Mizuta <dani.mizuta@gmail.com>
2025-03-08 10:36:10 -0300

    feat:discount

--- index.js
+++ index.js
@@ -928,6 +928,7 @@ app.post("/submit-order", async (req, res) => {
         }
 
         res.status(200).send({ message: "Notes and discount updated successfully!" });
+        console.log('DESCONTO:', discount);
     } catch (error) {
         console.error("Error updating notes and discount:", error);
         res.status(500).send({ error: "Failed to update order." });
